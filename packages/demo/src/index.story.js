c05bd8aa34520ef49164f85b78f232cd033d1d0c
Davide Fiorello <davide@codeflyer.com>
2018-06-17 13:15:03 +0200

    feat(demo): added first implementation

---
+++ packages/demo/src/index.story.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+import {
+  SignInCredentials,
+  SignInChangePassword,
+  SignInValidateWithCode
+} from 'auth-n-roll-components-styled-components'
+
+import { AuthNRollProvider, SignIn, SignInCredentialElement } from 'auth-n-roll'
+
+import { ServiceInMemory, ServiceSwitch } from 'auth-n-roll-dev-tools'
+import stackData from '../../../data/stack'
+
+import { ServiceCognito } from 'auth-n-roll-service-cognito'
+
+const styles = {
+  wrapper: { width: '300px' }
+}
+storiesOf('Demo', module)
+  .add('Signin Component', () => {
+    return (
+      <AuthNRollProvider authService={ServiceCognito(stackData)}>
+        <div style={styles.wrapper}>
+          <SignInCredentials />
+        </div>
+      </AuthNRollProvider>
+    )
+  })
+  .add('Signin Page In Memory', () => {
+    return (
+      <AuthNRollProvider authService={ServiceInMemory()} debug>
+        <div style={styles.wrapper}>
+          <SignIn>
+            <SignInCredentials index={SignIn.FLOW_STEP_CREDENTIAL} />
+            <SignInChangePassword index={SignIn.FLOW_STEP_CHANGE_PASSWORD} />
+            <SignInValidateWithCode index={SignIn.FLOW_STEP_VALIDATE_CODE} />
+          </SignIn>
+          <ServiceSwitch />
+        </div>
+      </AuthNRollProvider>
+    )
+  })
+  .add('Signin Page Cognito', () => {
+    return (
+      <AuthNRollProvider authService={ServiceCognito(stackData)} debug>
+        <div style={styles.wrapper}>
+          <SignIn>
+            <SignInCredentials index={SignIn.FLOW_STEP_CREDENTIAL} />
+            <SignInChangePassword index={SignIn.FLOW_STEP_CHANGE_PASSWORD} />
+            <SignInValidateWithCode index={SignIn.FLOW_STEP_VALIDATE_CODE} />
+          </SignIn>
+          <ServiceSwitch />
+        </div>
+      </AuthNRollProvider>
+    )
+  })
